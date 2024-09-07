823a5f0b630f75f58004cadb9e9ea605893d46c4
toasted-cornkernels <volcano0909@gmail.com>
2024-09-07 15:55:25 -0700

    oh no it's borken

---
+++ packages/Utils/ELispMachineStraight.el
@@ -0,0 +1,24 @@
+;; Straight =========================================
+;; ==================================================
+
+(defvar bootstrap-version)
+
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+	(url-retrieve-synchronously
+	 "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el" 'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t
+      package-install-upgrade-built-in t)
+
+(use-package use-package-ensure-system-package :ensure t)
+
+(provide 'ELispMachine/straight)
