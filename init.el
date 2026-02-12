c37db828cc0c01d20edbd13a467abe4cfa6ccc4c
Jeongsoo Lee <jeongsoolee09@github.com>
2026-02-12 10:49:49 -0500

    update

--- init.el
+++ init.el
@@ -1622,24 +1622,18 @@
         gptel-backend (gptel-make-gh-copilot "Copilot")
         gptel-default-mode 'org-mode)
   :general-config
-  (agnostic-key
-    :major-modes '(gptel-mode t)
-    :keymaps     '(gptel-mode-map)
-    "C-c C-c"    'gptel-send
-    "C-c C-q"    'gptel-abort)
   (local-leader
     :major-modes '(gptel-mode t)
     :keymaps     '(gptel-mode-map)
-    ","          'gptel-send
-    "q"          'gptel-abort
-    "m"          'gptel-menu
-    ;;; Capitalized to avoid clash with org-mode-map
-    "A"          'gptel-add
-    "F"          'gptel-add-file
-    "O"          'gptel-org-set-topic
-    "P"          'gptel-org-set-properties
-    ;; "R"          'gptel-rewrite
-    )
+    "g"          (which-key-prefix "gptel")
+    "gs"         'gptel-send
+    "gq"         'gptel-abort
+    "gg"         'gptel-menu
+    "ga"         'gptel-add
+    "gf"         'gptel-add-file
+    "gt"         'gptel-org-set-topic
+    "gp"         'gptel-org-set-properties
+    "gr"         'gptel-rewrite)
   (normal-mode-major-mode
     :major-modes '(gptel-context-buffer-mode t)
     :keymaps     '(gptel-context-buffer-mode-map)
