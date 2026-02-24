362c3b6d3ee2b489f762b28fee680b63170dad50
toasted-cornkernels <volcano0909@gmail.com>
2026-02-23 21:10:30 -0500

    update

--- init.el
+++ init.el
@@ -544,10 +544,10 @@
 ;; Emoji config =====================================
 ;; ==================================================
 
-;; (use-package emojify
-;;   :hook (after-init . global-emojify-mode)
-;;   :init
-;;   (setq emojify-emoji-styles '(unicode github)))
+(use-package emojify
+  :defer t
+  :init
+  (setq emojify-emoji-styles '(unicode github)))
 
 ;; Align ============================================
 ;; ==================================================
@@ -4571,6 +4571,59 @@ set so that it clears the whole REPL buffer, not just the output."
 ;; ==================================================
 
 (use-package consult
+  :bind (;; C-c bindings in `mode-specific-map'
+         ("C-c M-x" . consult-mode-command)
+         ("C-c h" . consult-history)
+         ("C-c k" . consult-kmacro)
+         ("C-c m" . consult-man)
+         ("C-c i" . consult-info)
+         ([remap Info-search] . consult-info)
+         ;; C-x bindings in `ctl-x-map'
+         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+         ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+         ;; M-g bindings in `goto-map'
+         ("M-g e" . consult-compile-error)
+         ("M-g r" . consult-grep-match)
+         ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
+         ("M-g g" . consult-goto-line)             ;; orig. goto-line
+         ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+         ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings in `search-map'
+         ("M-s d" . consult-find)                  ;; Alternative: consult-fd
+         ("M-s c" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch-history)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
+         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
+         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
+         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
+         ;; Minibuffer history
+         :map minibuffer-local-map
+         ("M-s" . consult-history)                 ;; orig. next-matching-history-element
+         ("M-r" . consult-history))
   :hook
   (completion-list-mode . consult-preview-at-point-mode)
   :init
@@ -5346,7 +5399,9 @@ set so that it clears the whole REPL buffer, not just the output."
 (use-package uniquify
   :straight nil
   :config
-  (setq uniquify-buffer-name-style 'forward))
+  (setq uniquify-buffer-name-style 'post-forward-angle-brackets
+        ;; don't screw special buffers
+        uniquify-ignore-buffers-re "^\\*"))
 
 ;; Helpful ==========================================
 ;; ==================================================
@@ -5920,6 +5975,9 @@ set so that it clears the whole REPL buffer, not just the output."
   "ws" 'evil-window-split
   "wv" 'evil-window-vsplit
 
+  "wf" 'follow-mode
+  "ww" 'other-window
+
   "wM" 'ace-swap-window
   "wS" 'ace-swap-window
 
@@ -5936,6 +5994,7 @@ set so that it clears the whole REPL buffer, not just the output."
   "wU" 'winner-redo
   "w;" 'evil-window-vsplit
   "w'" 'evil-window-split
+  "wx" 'kill-buffer-and-window
 
   ";"  'evil-window-vsplit
   ":"  'eval-expression
