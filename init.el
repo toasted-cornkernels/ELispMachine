f827606a50e66cae6712aa164c15e0c2a4784669
Jeongsoo Lee <jeongsoolee09@github.com>
2026-02-24 10:35:43 -0500

    update

--- init.el
+++ init.el
@@ -5332,7 +5332,40 @@ set so that it clears the whole REPL buffer, not just the output."
 ;; ==================================================
 
 (use-package consult-gh
-  :defer t)
+  :after consult
+  :custom
+  (consult-gh-show-preview t)
+  (consult-gh-preview-key "C-o")
+  (consult-gh-repo-action #'consult-gh--repo-browse-files-action)
+  (consult-gh-large-file-warning-threshold 2500000)
+  (consult-gh-confirm-name-before-fork nil)
+  (consult-gh-confirm-before-clone t)
+  (consult-gh-notifications-show-unread-only nil)
+  (consult-gh-default-interactive-command #'consult-gh-transient)
+  (consult-gh-prioritize-local-folder nil)
+  (consult-gh-group-dashboard-by :reason)
+  ;;;; Optional
+  (consult-gh-repo-preview-major-mode nil) ; show readmes in their original format
+  (consult-gh-preview-major-mode 'org-mode) ; use 'org-mode for editing comments, commit messages, ...
+  :config
+  ;; Remember visited orgs and repos across sessions
+  (add-to-list 'savehist-additional-variables 'consult-gh--known-orgs-list)
+  (add-to-list 'savehist-additional-variables 'consult-gh--known-repos-list)
+  ;; Enable default keybindings (e.g. for commenting on issues, prs, ...)
+  (consult-gh-enable-default-keybindings))
+
+
+;; Install `consult-gh-embark' for embark actions
+(use-package consult-gh-embark
+  :config
+  (consult-gh-embark-mode +1))
+
+
+;; Install `consult-gh-forge' for forge actions
+(use-package consult-gh-forge
+ :config
+ (consult-gh-forge-mode +1)
+ (setq consult-gh-forge-timeout-seconds 20))
 
 ;; format-all =======================================
 ;; ==================================================
