995a2acf9b1751dffbce9c0b659753422ffcba37
toasted-cornkernels <volcano0909@gmail.com>
2026-02-24 21:38:09 -0500

    Add corfu, cape, and remove company

--- init.el
+++ init.el
@@ -4530,6 +4530,24 @@ set so that it clears the whole REPL buffer, not just the output."
 
 (use-package emacs
   :straight nil
+  ;; ==================== corfu ====================
+  :custom
+  ;; TAB cycle if there are only few candidates
+  ;; (completion-cycle-threshold 3)
+
+  ;; Enable indentation+completion using the TAB key.
+  ;; `completion-at-point' is often bound to M-TAB.
+  (tab-always-indent 'complete)
+
+  ;; Emacs 30 and newer: Disable Ispell completion function.
+  ;; Try `cape-dict' as an alternative.
+  (text-mode-ispell-word-completion nil)
+
+  ;; Hide commands in M-x which do not apply to the current mode.  Corfu
+  ;; commands are hidden, since they are not used via M-x. This setting is
+  ;; useful beyond Corfu.
+  (read-extended-command-predicate #'command-completion-default-include-p)
+  ;; ===============================================
   :init
   (defun crm-indicator (args)
     (cons (format "[CRM%s] %s"
@@ -4697,34 +4715,86 @@ set so that it clears the whole REPL buffer, not just the output."
 ;; Company config ===================================
 ;; ==================================================
 
-(use-package company
-  :hook ((prog-mode . global-company-mode)
-         (org-mode  . global-company-mode))
-  :commands (company-mode)
-  :config
-  ;; (global-company-mode)
-  (setq company-idle-delay 0.05
-        company-echo-delay 0.05
-        company-tooltip-idle-delay 0
-        company-async-redisplay-delay 0
-        company-dabbrev-downcase nil)
-  (define-key company-active-map (kbd "<return>") nil)
-  (define-key company-active-map (kbd "RET") nil)
-  (define-key company-active-map (kbd "C-h") nil)
-  (define-key company-active-map (kbd "<tab>") #'company-complete-selection)
-  (define-key company-active-map (kbd "TAB") #'company-complete-selection))
+;; (use-package company
+;;   :hook ((prog-mode . global-company-mode)
+;;          (org-mode  . global-company-mode))
+;;   :commands (company-mode)
+;;   :config
+;;   ;; (global-company-mode)
+;;   (setq company-idle-delay 0.05
+;;         company-echo-delay 0.05
+;;         company-tooltip-idle-delay 0
+;;         company-async-redisplay-delay 0
+;;         company-dabbrev-downcase nil)
+;;   (define-key company-active-map (kbd "<return>") nil)
+;;   (define-key company-active-map (kbd "RET") nil)
+;;   (define-key company-active-map (kbd "C-h") nil)
+;;   (define-key company-active-map (kbd "<tab>") #'company-complete-selection)
+;;   (define-key company-active-map (kbd "TAB") #'company-complete-selection))
 
-(use-package company-lua
-  :after (company lua-mode))
+;; (use-package company-lua
+;;   :after (company lua-mode))
 
-(use-package company-web
-  :after (company web-mode))
+;; (use-package company-web
+;;   :after (company web-mode))
 
-(use-package company-auctex
-  :after (company tex))
+;; (use-package company-auctex
+;;   :after (company tex))
 
-(use-package company-emojify
-  :after (company emojify))
+;; (use-package company-emojify
+;;   :after (company emojify))
+
+;; Corfu config =====================================
+;; ==================================================
+
+(use-package corfu
+  ;; Optional customizations
+  ;; :custom
+  ;; (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-preselect 'prompt)      ;; Preselect the prompt
+  ;; (corfu-on-exact-match 'insert) ;; Configure handling of exact matches
+
+  ;; Enable Corfu only for certain modes. See also `global-corfu-modes'.
+  ;; :hook ((prog-mode . corfu-mode)
+  ;;        (shell-mode . corfu-mode)
+  ;;        (eshell-mode . corfu-mode))
+
+  :init
+
+  ;; Recommended: Enable Corfu globally.  Recommended since many modes provide
+  ;; Capfs and Dabbrev can be used globally (M-/).  See also the customization
+  ;; variable `global-corfu-modes' to exclude certain modes.
+  (global-corfu-mode)
+
+  ;; Enable optional extension modes:
+  ;; (corfu-history-mode)
+  ;; (corfu-popupinfo-mode)
+  )
+
+;; Add extensions
+(use-package cape
+  ;; Bind prefix keymap providing all Cape commands under a mnemonic key.
+  ;; Press C-c p ? to for help.
+  :bind ("C-c p" . cape-prefix-map) ;; Alternative key: M-<tab>, M-p, M-+
+  ;; Alternatively bind Cape commands individually.
+  ;; :bind (("C-c p d" . cape-dabbrev)
+  ;;        ("C-c p h" . cape-history)
+  ;;        ("C-c p f" . cape-file)
+  ;;        ...)
+  :init
+  ;; Add to the global default value of `completion-at-point-functions' which is
+  ;; used by `completion-at-point'.  The order of the functions matters, the
+  ;; first function returning a result wins.  Note that the list of buffer-local
+  ;; completion functions takes precedence over the global list.
+  (add-hook 'completion-at-point-functions #'cape-dabbrev)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  ;; (add-hook 'completion-at-point-functions #'cape-history)
+  ;; ...
+)
 
 ;; Embark config ====================================
 ;; ==================================================
