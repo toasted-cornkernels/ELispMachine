7aaf2cb24308daa71b4a5016bd4d4185d3dba60e
toasted-cornkernels <volcano0909@gmail.com>
2026-02-24 20:35:47 -0500

    update

--- init.el
+++ init.el
@@ -5368,18 +5368,9 @@ set so that it clears the whole REPL buffer, not just the output."
   ;; Enable default keybindings (e.g. for commenting on issues, prs, ...)
   (consult-gh-enable-default-keybindings))
 
-
-;; Install `consult-gh-embark' for embark actions
 (use-package consult-gh-embark
   :config
   (consult-gh-embark-mode +1))
-
-
-;; ;; Install `consult-gh-forge' for forge actions
-;; (use-package consult-gh-forge
-;;  :config
-;;  (consult-gh-forge-mode +1)
-;;  (setq consult-gh-forge-timeout-seconds 20))
 
 ;; format-all =======================================
 ;; ==================================================
